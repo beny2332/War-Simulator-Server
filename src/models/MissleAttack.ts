908068a99e6233bdd73ca777b912b4332f27969c
BinyaminYacovson <bj57577@gmail.com>
2024-11-13 14:08:43 +0200

    refactor missile attack model to use enum for status and add attack status enum

--- src/models/MissleAttack.ts
+++ src/models/MissleAttack.ts
@@ -1,17 +1,18 @@
 import { Schema, Types, Document, model } from "mongoose";
+import { Status } from "../types/enums/attackStatusEnum"
 
 // Define the Missile Attack interface
 export interface IMissileAttack extends Document {
   targetOrganization: Types.ObjectId;
   missileType: Types.ObjectId;
-  status: 'launched' | 'intercepted' | 'hit';
+  status: Status;
 }
 
 // Define the Missile Attack schema
 const missileAttackSchema = new Schema<IMissileAttack>({
   targetOrganization: { type: Schema.Types.ObjectId, ref: 'Organization', required: true },
   missileType: { type: Schema.Types.ObjectId, ref: 'Missile', required: true },
-  status: { type: String, enum: ['launched', 'intercepted', 'hit'], required: true }
+  status: { type: String, Status, required: true }
 });
 
 // Export the Missile Attack model
---
+++ src/types/enums/attackStatusEnum.ts
@@ -0,0 +1,5 @@
+export enum Status{
+    Launched = "Launched",
+    Intercepted = "Intercepted",
+    Hit = "Hit"
+}
